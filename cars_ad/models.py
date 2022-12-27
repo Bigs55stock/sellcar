9b48c673533a2a04146254d56060bf6cf3a5dc2f
humza15 <humzazafarullah@gmail.com>
2022-12-27 14:01:55 -0500

    cars model

--- cars_ad/models.py
+++ cars_ad/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 class Cars(models.Model):
 
-    Make = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
-    price = models.TextField(max_length=500)
+    price = models.TextField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+     def __str__(self):
+        return self.name
