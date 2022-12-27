3d5b63541bf843ca4ac0adae5820604aa2ae1f75
humza15 <humzazafarullah@gmail.com>
2022-12-27 14:59:54 -0500

    superuser

--- cars_ad/urls.py
+++ cars_ad/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"),
+     path('admin/', admin.site.urls),
+     path('', views.Home.as_view(), name="home"),
      path('about/', views.About.as_view(), name="about"),
      path('cars/', views.Carslist.as_view(), name="cars_list")
 ]
