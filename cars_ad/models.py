62f3421a7827aaa20d9d22b8ad7092391771e913
humza15 <humzazafarullah@gmail.com>
2022-12-27 12:54:51 -0500

    model for car

--- cars_ad/models.py
+++ cars_ad/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Cars(models.Model):
+
+    Make = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    price = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
