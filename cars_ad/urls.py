a95bb218d500f10bbe4936af0fbf2fc063e195be
humza15 <humzazafarullah@gmail.com>
2022-12-27 15:14:32 -0500

    quick save

--- cars_ad/urls.py
+++ cars_ad/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
+    
      path('', views.Home.as_view(), name="home"),
      path('about/', views.About.as_view(), name="about"),
      path('cars/', views.Carslist.as_view(), name="cars_list")
