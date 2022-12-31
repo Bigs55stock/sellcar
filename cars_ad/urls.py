69b20a0d5e0192e7be8d552fa816df94c54ac020
humza15 <humzazafarullah@gmail.com>
2022-12-31 11:13:05 -0500

    route ad

--- cars_ad/urls.py
+++ cars_ad/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
      path('', views.Home.as_view(), name="home"),
      path('about/', views.About.as_view(), name="about"),
      path('cars/', views.Carslist.as_view(), name="cars_list")
+     path('cars/new/', views.CarCreate.as_view(), name="car_create")
 ]
