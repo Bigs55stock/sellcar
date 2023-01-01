625d2d230a076bb407527865d55b97094c31feea
humza15 <humzazafarullah@gmail.com>
2023-01-01 15:23:46 -0500

    user inden added

--- cars_ad/models.py
+++ cars_ad/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Cars(models.Model):
@@ -9,7 +9,7 @@ class Cars(models.Model):
     desc = models.CharField(max_length=500)
     img = models.CharField(max_length=500)
     price = models.TextField(max_length=100)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
 
     def __str__(self):
--- cars_ad/views.py
+++ cars_ad/views.py
@@ -1,10 +1,16 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View 
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import Cars
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
 # Create your views here.
 
 
@@ -14,6 +20,7 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
+@method_decorator(login_required, name='dispatch')
 class Carslist(TemplateView):
     template_name = "cars_list.html"
       
@@ -21,18 +28,25 @@ class Carslist(TemplateView):
         context = super().get_context_data(**kwargs)
         make = self.request.GET.get("make")
         if make != None:
-            context["cars"] = Cars.objects.filter(make__icontains=make)
+            context["cars"] = Cars.objects.filter(make__icontains=make, user=self.request.user)
         else:
-            context["cars"] = Cars.objects.all()
+            context["cars"] = Cars.objects.filter(user=self.request.user)
         return context
    
-
-   
+@method_decorator(login_required, name='dispatch')
 class CarCreate(CreateView):
     model = Cars
     fields = ['make','year','model' ,'desc', 'img', 'price']
     template_name = "car_create.html"
     success_url = "/cars/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CarCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('car_detail', kwargs={'pk': self.object.pk})
       
 class CarDetail(DetailView):
     model = Cars
@@ -50,3 +64,19 @@ class CarDelete(DeleteView):
     model = Cars
     template_name = "car_delete.html"
     success_url = "/cars/"
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+   
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("artist_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
