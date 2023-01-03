6243fced7502c9ab4164ad25a15f73898c2c389f
humza15 <humzazafarullah@gmail.com>
2023-01-03 09:42:31 -0500

    forgot to com migrations, model edit, homepage

--- cars_ad/models.py
+++ cars_ad/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+ 
+ 
 class Cars(models.Model):
-    make = models.CharField(max_length=150)
-    year = models.CharField(max_length=150)
-    model = models.CharField(max_length=150)
-    desc = models.CharField(max_length=500)
-    img = models.CharField(max_length=500)
-    price = models.TextField(max_length=100)
+    make = models.CharField(max_length=150, default="")
+    year = models.CharField(max_length=150,default="")
+    model = models.CharField(max_length=150,default="")
+    price = models.CharField(max_length=300, default="")
+    mileage = models.CharField(max_length=300, default="")
+    img = models.CharField(max_length=500, default="")
+    desc = models.TextField(max_length=500,default="")
+    contact = models.CharField(max_length=100, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
 
